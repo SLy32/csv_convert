4b6cae1d81514f57c2b776ebd51355b3dfb01d94
slava <sly.kazakoff@gmail.com>
2019-02-02 12:21:57 +0300

    full copy extend data

--- convert.py
+++ convert.py
@@ -138,45 +138,48 @@ class Convert:
                         writer.writerow(header)
 
     def full_copy(self):
-        return
         with Profiler() as p:
             row = self.lazy()
             with open(f"{self.output_file}", "w", newline='') as output_csv:
                 writer = csv.writer(output_csv, delimiter='\t')
-
                 for count, item in enumerate(row):
-                    if count > 0:
+                    print(f"Row: {count}")
+                    if count == 0:
                         data = item[0].split('\t')
+                        print(data)
+                        header = []
+                        header.insert(0, 'chrom')
+                        header.insert(1, 'pos')
+                        header.insert(2, 'ref')
+                        header.insert(3, 'alt')
+                        header.extend(data)
+                        print(header)
+                        for i, item in enumerate(header):
+                            if item == 'minor_AF':
+                                header.pop(i)
+                                header.insert(i, 'MAF')
+                        print(header)
+                        writer.writerow(header)
+                        print(len(header))
+                    else:
+                        data = item[0].split('\t')
+                        print(len(data))
+                        print(data)
                         dtw = []
                         dt = data[0].split(':')
                         dtw.insert(0, dt[0])
                         dtw.insert(1, dt[1])
                         dtw.insert(2, dt[2])
                         dtw.insert(3, dt[3])
-                        dtw.insert(4, data[2])
-                        dtw.insert(5, data[11])
+                        dtw.extend(data)
                         writer.writerow(dtw)
-                        if count > 2000:
-                            break
-                    else:
-                        header = []
-                        header.insert(0, 'chrom')
-                        header.insert(1, 'pos')
-                        header.insert(2, 'ref')
-                        header.insert(3, 'alt')
-                        header.insert(4, 'MAF')
-                        header.insert(5, 'pval')
-                        print(header)
-                        print(len(item[0].split('\t')))
-                        writer.writerow(header)
-
-        # with open("Footballers.csv") as f:
-        #     reader = csv.reader(f)
-        #     print(next(reader)[0])  # default separator is a comma so you only get one item in the row
+                        print(len(dtw))
+                    if count > 5:
+                        break
 
 if __name__ == "__main__":
     path_to = f"{os.path.dirname(os.path.abspath(__file__))}"
-    file = 'age.gwas.imputed_v3.male.tsv/age.gwas.imputed_v3.male.tsv'
+    file = 'age.gwas.imputed_v3.both_sexes.tsv'
     convert = Convert(file)
     print(path_to, file)
     convert.full_copy()
